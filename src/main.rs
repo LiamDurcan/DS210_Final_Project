dcff79cdae04a1d8dc892d810e0108850570b96e
Liam Durcan <ldurcan@bu.edu>
2025-04-30 22:50:43 -0400

    Added rand and randomly split data & labels into training and testing data. 20% of the data (100 rows) will be used for testing.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod knn;
 mod reader;
 use reader::DataFrame;
 use crate::reader::ColumnVal;
+//use rand::Rng; originally used this to split the data
+use rand::seq::SliceRandom;
 fn main() {
     let types = vec![3,1,3,3,3,3,3,2,2,2];
     let mut df = DataFrame::new();
@@ -16,6 +18,7 @@ fn main() {
     numerical_data_df.restrict_columns(data);
     labels_df.restrict_columns(label);
 
+    //Turn the numerical_data_df into a Vector of vectors from its original struct form
     let mut numerical_rows_vec:Vec<Vec<f64>> = vec![vec![]; numerical_data_df.num_rows];
     for column in numerical_data_df.column_order{
         let column_data = numerical_data_df.columns.get(&column).unwrap();
@@ -29,4 +32,42 @@ fn main() {
         }
     }
     println!("{:?}", numerical_rows_vec[0]);
+
+    //Convert the labels to Vec<bool> from the labels_df dataframe.
+    let mut labels: Vec<bool> = Vec::new();
+    for i in 0..labels_df.num_rows{
+        match &labels_df.columns.get(&labels_df.column_order[0]).expect("Column not found.")[i]{
+            ColumnVal::Two(value) => labels.push(*value),
+            _ => panic!("Expected boolean label"),
+        }
+    }
+
+    //Split the data so that 20% is test data, 80% is training data
+    /*
+    let mut rng = rand::thread_rng();
+    let random_num = rng.gen_range(1..=5); //Randomly select 1 in every 5 rows
+    let mut numerical_test_rows:Vec<Vec<f64>> = Vec::new();
+    let mut numerical_train_rows:Vec<Vec<f64>> = Vec::new();
+    let mut test_labels: Vec<bool> = Vec::new();
+    let mut train_labels:Vec<bool> = Vec::new();
+    for i in 0..numerical_rows_vec.len(){
+        if i%5 == random_num{//20% of data will become test data, the rest is training data
+            numerical_test_rows.push(numerical_rows_vec[i].clone());
+            test_labels.push(labels[i]);
+        }
+        else{
+            numerical_train_rows.push(numerical_rows_vec[i].clone());
+            train_labels.push(labels[i]);
+        }
+    }*/
+    let mut rng = rand::thread_rng();//needed for shuffle function.
+    let mut data_label_pairs: Vec<(Vec<f64>, bool)> = numerical_rows_vec.into_iter().zip(labels.into_iter()).collect();
+    data_label_pairs.shuffle(&mut rng); // shuffle the paired data
+    
+    let test_size = (data_label_pairs.len() as f64 * 0.2).round() as usize;
+    let (test_data, train_data) = data_label_pairs.split_at(test_size);
+    
+    // Unzip back into features and labels
+    let (numerical_test_rows, test_labels): (Vec<_>, Vec<_>) = test_data.iter().cloned().unzip();
+    let (numerical_train_rows, train_labels): (Vec<_>, Vec<_>) = train_data.iter().cloned().unzip();
 }
